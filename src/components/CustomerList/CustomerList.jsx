b9b446fc1b3468b23b2500c974f7468f61ac3597
omersabunccu <omersabunccu@gmail.com>
2024-01-14 15:10:21 -0500

    first commit

---
+++ src/components/CustomerList/CustomerList.jsx
@@ -0,0 +1,24 @@
+
+
+import CustomerItem from '../CustomerItem/CustomerItem'
+import './CustomerList.css'
+
+
+
+
+const CustomerList = ({customers, setCustomers}) => {
+
+  const handleDelete = (item) => {
+    setCustomers(customers.filter((customer) => customer.id !== item.id))
+  }
+
+  return (
+    <ul className='customer-list'>
+        {customers.map((customer) => (
+          <CustomerItem customer={customer} key={customer.id} handleDelete={handleDelete}/>
+        ))}
+    </ul>
+  )
+}
+
+export default CustomerList
